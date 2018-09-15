8e511b29b528e22dcd5ca9b8a6551d0f4e0b1fc1
Htu_D <3170102733@zju.edu.cn>
2018-09-15 19:37:29 +0800

    two methods

--- main.py
+++ main.py
@@ -31,19 +31,11 @@ obpoint = np.zeros((9 * 6, 3), np.float32)
 obpoint[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
 # obpoint *= 30
 objpoints = []
-imgpoints = []
+imgpoints1 = []
+imgpoints2 = []
 tempimg = cv.imread('data\left\left01.jpg')
 
-
 # print(obpoint)
-
-
-def loadimg(img):
-    # grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    retval, corners = cv.findChessboardCorners(img, inner_conner_num)
-    # corners2 = cv.cornerSubPix(img, corners, (10, 10), (-1, -1), criteria)
-    objpoints.append(obpoint)
-    imgpoints.append(corners)
 
 
 # load images which is listed in data.txt
@@ -54,9 +46,15 @@ with open('data.txt', 'r') as data:
         # only process the left data
         if datatype == '0':
             tempimg = cv.imread(path)
-            loadimg(tempimg)
+            # grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            retval, corners = cv.findChessboardCorners(tempimg, inner_conner_num)
+            # corners2 = cv.cornerSubPix(img, corners, (10, 10), (-1, -1), criteria)
+            objpoints.append(obpoint)
+            imgpoints1.append(corners)
+
     # camera calibration
-    retvalL, camtxL, distL, rvecsL, tvecsL = cv.calibrateCamera(objpoints, imgpoints, tempimg.shape[:2], None, None)
+    retvalL, camtxL, distL, rvecsL, tvecsL = cv.calibrateCamera(objpoints, imgpoints1,
+                                                                tempimg.shape[:2], None, None)
     np.save("camera_matrixL", camtxL)  # Internal parameter matrix
     np.save("distortion_cofficientsL", distL)  # distortion cofficients = (k_1,k_2,p_1,p_2,k_3)
     np.save("rotation_vectorsL", rvecsL)  # rotation vectors
@@ -69,42 +67,53 @@ with open('data.txt', 'r') as data:
         # only process the left data
         if datatype == '1':
             tempimg = cv.imread(path)
-            loadimg(tempimg)
+            # grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            retval, corners = cv.findChessboardCorners(tempimg, inner_conner_num)
+            # corners2 = cv.cornerSubPix(img, corners, (10, 10), (-1, -1), criteria)
+            imgpoints2.append(corners)
     # camera calibration
-    retvalR, camtxR, distR, rvecsR, tvecsR = cv.calibrateCamera(objpoints, imgpoints, tempimg.shape[:2], None, None)
+    # print(tempimg.shape)
+    retvalR, camtxR, distR, rvecsR, tvecsR = cv.calibrateCamera(objpoints, imgpoints2,
+                                                                tempimg.shape[:2], None, None)
     np.save("camera_matrixR", camtxR)  # Internal parameter matrix
     np.save("distortion_cofficientsR", distR)  # distortion cofficients = (k_1,k_2,p_1,p_2,k_3)
     np.save("rotation_vectorsR", rvecsR)  # rotation vectors
     np.save("translation_vectorsR", tvecsR)  # translation vectors
 rmtxset = []
-tvecset=[]
+tvecset = []
 for n in range(12):
     rmtxL = cv.Rodrigues(rvecsL[n])
     rmtxR = cv.Rodrigues(rvecsR[n])
     rmtx = np.dot(rmtxR[0], rmtxL[0].T)
-    tvec=tvecsR[n]-np.dot(rmtx,tvecsL[n])
+    tvec = tvecsR[n] - np.dot(rmtx, tvecsL[n])
     # print(tvec)
     rmtxset.append(rmtx)
     tvecset.append(tvec)
-    np.save("rotation_mat",rmtxset)
-    np.save("teanslation_vectors",tvecset)
+rmtx=np.mean(rmtxset,0)
+tvec=np.mean(tvecset,0)
+np.save("rotation_mat", rmtx)
+np.save("translation_vectors", tvec)
+ret, camtx, distL, camtxR, distR, R, T, E, F = cv.stereoCalibrate(objpoints, imgpoints1,
+                                                                   imgpoints2, camtxL, distL, camtxR,
+                                                                   distR, tempimg.shape[:2])
+np.save("rotation_mat0", R)
+np.save("translation_vectors0", T)
 # undistort
 # for line in lines:
 #     path, datatype = line.rstrip().split(',')
 #     if datatype == '0':
 #         tempimg = cv.imread(path)
-#         new_camtx, roi = cv.getOptimalNewCameraMatrix(camtx, dist, tempimg.shape[:2], 0)
-#         dstimg = cv.undistort(tempimg, camtx, dist, None, new_camtx)
+#         new_camtx, roi = cv.getOptimalNewCameraMatrix(camtxL, distL, tempimg.shape[:2], 0)
+#         dstimg = cv.undistort(tempimg, camtxL, distL, None, new_camtx)
 #         # save at a new folder
 #         path1, path2, path3 = path.split('\\')
 #         new_path = '{}\\new{}\{}'.format(path1, path2, path3)
 #         cv.imwrite(new_path, dstimg)
-
-
-# calculate the error of the process above:0.04965026811058618
+#
+# # calculate the error of the process above:0.04965026811058618
 # total_error = 0
 # for i in range(len(objpoints)):
-#     imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], camtx, dist)
+#     imgpoints2, _ = cv.projectPoints(objpoints[i], rvecsL[i], tvecsL[i], camtxL, distL)
 #     error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2) / len(imgpoints2)
 #     total_error += error
 # print("total error: ", total_error / len(objpoints))
